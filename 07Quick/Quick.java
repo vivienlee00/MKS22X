ad6d59f470ea3d04e6ad71d98f6a61853c20e17f
Vivien Lee <Vivien Lee>
2017-03-16 22:46:36 -0400

    it seems to be working i think?????????????????

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,21 +1,36 @@
 import java.util.*;
 
 
-public class partition{
+public class Quick{
 
-    public static int getKth(int[] data, int index){
-	return 0;
+    public static int quickselect(int[] data, int k){
+	return partHelp(data,k,0,data.length-1);
+    }
+
+
+    public static int partHelp(int[]data, int k, int start, int end){
+
+	int ans = part(data,start,end);
+
+	if(ans < k){
+	    return partHelp(data,k,ans+1,end);
+	}
+	
+	if(ans > k){
+	    return partHelp(data,k,start,ans-1);
+	}
+
+	return data[ans];
+	
     }
 
     public static int part(int[] data, int start, int end){
 
 	Random randgen = new Random();
-	int pivotIndex  = randgen.nextInt((end-start)+1);
-	int pivot = data[pivotIndex+start];
+       	int pivotIndex  = (randgen.nextInt((end-start)+1) + start);
+	int pivot = data[pivotIndex];
 
-	System.out.println("pivot" + pivot);
-
-       	int finalIndex = 0;
+       	int finalIndex = pivotIndex;
 
 	int left = start;
 	int right = end;
@@ -32,25 +47,27 @@ public class partition{
 	    }
 
 
+
 	    if(right>left){
 		int temp = data[left];
 		data[left] = data[right];
-		data[right] = temp;			       
+		data[right] = temp;
+
+		int tempIndex = finalIndex;
+		if(left == tempIndex){
+		    finalIndex = right;
+		}
+
+		if(right == tempIndex){
+		    finalIndex = left;
+		}
 
 		if(data[left] == data[right]){
 		    right-=1;
 		}
 
-		if(left == pivotIndex){
-		    finalIndex = right;
-		}
-
-		if(right == pivotIndex){
-		    finalIndex = left;
-		}
 	    }
 	}
-	//FIX THIS!!!!!!!!!!!!!
 	return finalIndex;
     }
 
@@ -61,6 +78,8 @@ public class partition{
 		str += data[j];
 		str += " ";
 	    }
+
+	    str+="]";
 
 	return str;
     }
@@ -76,21 +95,21 @@ public class partition{
 
 	*/
 	
-	int[] ary4 = {0,0,0,1,2,3};
+	int[] ary4 = {2,10,15,23,0,5};
 
 	/*
 	part(ary,0,ary.length-1);
 	part(ary2,0,ary2.length-1);
 	part(ary3,0,ary3.length-1);
-	*/
-        System.out.println(part(ary4,0,ary4.length-1));
+	
+        part(ary4,0,ary4.length-1;
 
-	/*
+	
 	System.out.println(toString(ary));
 	System.out.println(toString(ary2));
 	System.out.println(toString(ary3));
 	*/
-        System.out.println(toString(ary4));
-
+	//  System.out.println(toString(ary4));
+	System.out.println(quickselect(ary4,2));
     }
 }
