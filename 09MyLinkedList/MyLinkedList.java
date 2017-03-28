55078e7e96ea706f939d7c5d412b17076acb9607
Vivien Lee <Vivien Lee>
2017-03-28 19:22:18 -0400

    finished w phase 1

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -7,7 +7,8 @@ public class MyLinkedList{
 	size = 0;
     }
 
-    class LNode(){
+
+    class LNode{
 	private int value;
 	private LNode next = null;
 
@@ -22,16 +23,61 @@ public class MyLinkedList{
     }
 
     private boolean add(int value){
+	//EDIT: adds node to the front
 	LNode x  =  new LNode(value, this.start);
 	start = x;
 	size += 1;
+	return true;
     }
 
-    private int size(){
+    public int size(){
 	return this.size;
     }
 
-    private String toString(){
+    public String toString(){
+	String x = "[";
+	LNode current = start;
+	while(current != null){
+	    x+=(current.value + ",");
+	    current = current.next;
+	}
+	//EDIT: x+= backspace char + "]"
+	return x;
+    }
+
+    public int get(int index){
+	int ans = 0;
+	LNode current = start;
+	for(int i = 0; i < index+1; i++){
+	    ans = current.value;
+	    current = current.next;
+	}
+	return ans;
+    }
+
+    public int set(int index, int newValue){
+	LNode current = start;
+	for(int i = 0; i < index; i++){
+	    current = current.next;
+	}
+	int ans = current.value;
+	current.value = newValue;
+	return ans;
+    }
+
+    //indexOf; add(index, value); remove
+    //exceptions
+
+    public static void main(String[] args){
+	MyLinkedList x = new MyLinkedList();
+	x.add(5);
+	x.add(6);
+	x.add(5);
+	x.add(4);
+	x.add(3);
+	System.out.println(x + "  size=" +  x.size());
+	System.out.println(x.set(3,7));
+	System.out.println(x + "  size=" +  x.size());
 
     }
 }
